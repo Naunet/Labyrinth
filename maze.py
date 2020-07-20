b06e5a5ae4c98026279486223b45fce28f964eaf
Naunet <44895138+Naunet@users.noreply.github.com>
2020-07-20 18:49:44 +0200

    Make main a class

--- maze.py
+++ maze.py
@@ -8,102 +8,7 @@ import view
 import wall
 import score
 
-
-def handle_interaction(key):
-    """records player's interaction"""
-    anim = {'move': False, 'right': False, 'left': False,
-            'up': False, 'down': False, 'quit': False, 'pause': False}
-    if key == pygame.K_RIGHT:
-        anim['move'] = True
-        anim['right'] = True
-    if key == pygame.K_LEFT:
-        anim['move'] = True
-        anim['left'] = True
-    if key == pygame.K_UP:
-        anim['move'] = True
-        anim['up'] = True
-    if key == pygame.K_DOWN:
-        anim['move'] = True
-        anim['down'] = True
-    if key == pygame.K_q or key == pygame.K_ESCAPE:
-        anim['quit'] = True
-    if key == pygame.K_SPACE or key == pygame.K_PAUSE:
-        anim['pause'] = True
-    return anim
-
-
-def pause_game(old, change):
-    global pause_start, paused_time
-    if not old and change:
-        pygame.time.set_timer(LOOP_SHIFT, 0)
-        pause_start = pygame.time.get_ticks()
-        return True
-    if old and change:
-        pygame.time.set_timer(LOOP_SHIFT, TIMER)
-        paused_time += pygame.time.get_ticks() - pause_start
-        pause_start = None
-        return False
-    return old
-
-
-def run():
-    global close_game, paused, level_end, end_time
-    if event.type == LOOP_SHIFT:
-        maze.handle_shift()
-    if event.type == pygame.KEYDOWN:
-        key_flags.append(event.key)
-        pygame.time.set_timer(LOOP_KEY, 200)  # hold down loop
-    if event.type == pygame.KEYDOWN or event.type == LOOP_KEY:
-        for key in key_flags:
-            anim = handle_interaction(key)
-            close_game = anim['quit']
-            paused = pause_game(paused, anim['pause'])
-            maze.move(anim, paused)
-    if event.type == pygame.KEYUP:
-        key_flags.remove(event.key)
-        if not key_flags:
-            pygame.time.set_timer(LOOP_KEY, 0)
-
-    draw.header(current_level, paused, paused_time, level_end, end_time)
-    maze.draw()
-
-    if maze.is_end_game():
-        if not level_end:
-            end_time = pygame.time.get_ticks()-paused_time
-            level_end = True
-            paused = True
-            pygame.time.set_timer(LOOP_SHIFT, 0)
-            score.save(current_level, end_time)
-        draw.end(current_level, end_time)
-
-
-# define macro variables
-GRID_WIDTH = 20
-GRID_HEIGHT = 22
-if GRID_WIDTH < 5 or GRID_HEIGHT < 5:
-    raise Exception("Maze dimensions too small! \
-        ({}, {})".format(GRID_WIDTH, GRID_HEIGHT))
-TIMER = 1000
-LOOP_KEY = USEREVENT+2
-LOOP_SHIFT = USEREVENT+1
-
-# define global variables
-key_flags = list()
-current_level = 1
-level_end = False
-paused = False
-paused_time = 0
-pause_start = None
-end_time = None
-
-# setup
-pygame.time.set_timer(LOOP_SHIFT, TIMER)  # loop for shift
-draw = view.View(GRID_WIDTH, GRID_HEIGHT)
-maze = wall.Wall(draw, GRID_WIDTH, GRID_HEIGHT)
-
 # screen flag
-
-
 class Screen(Enum):
     MENU = 1
     SETTINGS = 2
@@ -112,32 +17,129 @@ class Screen(Enum):
     SANDBOX = 5
 
 
-screen = Screen.MENU
+class Game():
+    def __init__(self):
+        # define macro variables
+        self.LOOP_SHIFT = USEREVENT+1
+        self.LOOP_KEY = USEREVENT+2
 
-# program
-close_game = False
-while not close_game:
-    for event in [pygame.event.wait()] + pygame.event.get():
-        if event.type == pygame.QUIT:
-            close_game = True
-        if screen == Screen.MENU:
-            boxes = draw.menu()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                clicked = [index for index,
-                           b in enumerate(boxes) if b.collidepoint(pos)]
-                print(clicked)
-                if clicked:
-                    if clicked[0] == 0:
-                        screen = Screen.GAME  # LEVELS
-                    if clicked[0] == 1:
-                        screen = Screen.SANDBOX
-                    if clicked[0] == 2:
-                        screen = Screen.SETTINGS
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
-                    close_game = True
-        if screen == Screen.GAME:
-            run()
-        pygame.display.update()
-quit()
+        # define variables
+        self.key_flags = list()
+        self.current_level = 1
+        self.width = 20
+        self.height = 22
+        if self.width < 5 or self.height < 5:
+            raise Exception("Maze dimensions too small! \
+                ({}, {})".format(self.width, self.height))
+        self.timer = 1000
+        self.level_end = False
+        self.paused = False
+        self.paused_time = 0
+        self.pause_start = None
+        self.end_time = None
+
+        # setup
+        pygame.time.set_timer(self.LOOP_SHIFT, self.timer)  # loop for shift
+        self.draw = view.View(self.width, self.height)
+        self.maze = wall.Wall(self.draw, self.width, self.height)
+        self.screen = Screen.MENU
+        self.close_game = False
+
+        self.run()
+
+    def run(self):
+        while not self.close_game:
+            for event in [pygame.event.wait()] + pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.close_game = True
+                if self.screen == Screen.MENU:
+                    self.menu_select(event)
+                if self.screen == Screen.GAME:
+                    self.play(event)
+                pygame.display.update()
+        quit()
+
+    def menu_select(self, event):
+        boxes = self.draw.menu()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            clicked = [index for index,
+                       b in enumerate(boxes) if b.collidepoint(pos)]
+            print(clicked)
+            if clicked:
+                if clicked[0] == 0:
+                    self.screen = Screen.GAME  # LEVELS
+                if clicked[0] == 1:
+                    self.screen = Screen.SANDBOX
+                if clicked[0] == 2:
+                    self.screen = Screen.SETTINGS
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
+                self.close_game = True
+
+    def play(self, event):
+        if event.type == self.LOOP_SHIFT:
+            self.maze.handle_shift()
+        if event.type == pygame.KEYDOWN:
+            self.key_flags.append(event.key)
+            pygame.time.set_timer(self.LOOP_KEY, 200)  # hold down loop
+        if event.type == pygame.KEYDOWN or event.type == self.LOOP_KEY:
+            for key in self.key_flags:
+                anim = self.handle_interaction(key)
+                self.close_game = anim['quit']
+                self.paused = self.pause_game(self.paused, anim['pause'])
+                self.maze.move(anim, self.paused)
+        if event.type == pygame.KEYUP:
+            self.key_flags.remove(event.key)
+            if not self.key_flags:
+                pygame.time.set_timer(self.LOOP_KEY, 0)
+
+        self.draw.header(self.current_level, self.paused,
+                         self.paused_time, self.level_end, self.end_time)
+        self.maze.draw()
+
+        if self.maze.is_end_game():
+            if not self.level_end:
+                self.end_time = pygame.time.get_ticks()-self.paused_time
+                self.level_end = True
+                self.paused = True
+                pygame.time.set_timer(self.LOOP_SHIFT, 0)
+                score.save(self.current_level, self.end_time)
+            self.draw.end(self.current_level, self.end_time)
+
+    def handle_interaction(self, key):
+        """records player's interaction"""
+        anim = {'move': False, 'right': False, 'left': False,
+                'up': False, 'down': False, 'quit': False, 'pause': False}
+        if key == pygame.K_RIGHT:
+            anim['move'] = True
+            anim['right'] = True
+        if key == pygame.K_LEFT:
+            anim['move'] = True
+            anim['left'] = True
+        if key == pygame.K_UP:
+            anim['move'] = True
+            anim['up'] = True
+        if key == pygame.K_DOWN:
+            anim['move'] = True
+            anim['down'] = True
+        if key == pygame.K_q or key == pygame.K_ESCAPE:
+            anim['quit'] = True
+        if key == pygame.K_SPACE or key == pygame.K_PAUSE:
+            anim['pause'] = True
+        return anim
+
+    def pause_game(self, old, change):
+        if not old and change:
+            pygame.time.set_timer(self.LOOP_SHIFT, 0)
+            self.pause_start = pygame.time.get_ticks()
+            return True
+        if old and change:
+            pygame.time.set_timer(self.LOOP_SHIFT, self.timer)
+            self.paused_time += pygame.time.get_ticks() - self.pause_start
+            self.pause_start = None
+            return False
+        return old
+
+
+Game()
